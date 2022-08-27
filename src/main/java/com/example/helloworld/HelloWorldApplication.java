4e328e9a60e6c833f10b621d9bafccb825600793
ShubomNath-BL <shubhom.nath@bridgelabz.com>
2022-08-27 21:42:34 +0530

    Added: Hello message added

--- src/main/java/com/example/helloworld/HelloWorldApplication.java
+++ src/main/java/com/example/helloworld/HelloWorldApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class HelloWorldApplication {
 
     public static void main(String[] args) {
-
+        System.out.println("Hello to the spring boot world");
         SpringApplication.run(HelloWorldApplication.class, args);
     }
 
